2f36f69945168dcb521a081349eb1bb18df27314
Paul Rothenburger <linuxlearner@gmx.net>
2026-05-22 22:13:50 +0200

    Delete archive/django-prototype directory

--- archive/django-prototype/core/context_processors.py
+++
@@ -1,9 +0,0 @@
-from core.models import Gate, SiteMap
-
-
-def app_context(request):
-    return {
-        "active_gate_count": Gate.objects.filter(is_active=True).count(),
-        "active_site_map": SiteMap.objects.filter(is_active=True).order_by("-created_at").first(),
-    }
-
